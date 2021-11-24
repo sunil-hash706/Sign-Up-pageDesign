d783e41613c1d13d91feebb974ba0d1139100c33
suni <sunilkumarachra9461@gmail.com>
2021-11-24 14:37:44 +0530

    style edited

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ fn=()=>{
     console.log(a);
     if(pss.length<6){
         document.getElementById("shortPassword").style.visibility="visible";
+        document.getElementById("noName").style.visibility="hidden";
+        document.getElementById("noEmail").style.visibility="hidden";
     }
    else if(name==""){
-        console.log("hii");
         document.getElementById("noName").style.visibility="visible";
+        document.getElementById("noEmail").style.visibility="hidden";
+        document.getElementById("shortPassword").style.visibility="hidden";
     }
    else if(email==""){
         document.getElementById("noEmail").style.visibility="visible";
+        document.getElementById("noName").style.visibility="hidden";
+        document.getElementById("shortPassword").style.visibility="hidden";
     }
 
     else{
